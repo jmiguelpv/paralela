e69d676d983b50898587d9dc9cf8094ab9c27517
Miguel <miguelpadilla.v@gmail.com>
2020-09-24 12:59:35 -0500

    mas limpio proyecto2

--- parcial1/proyecto2/Filosofo.java
+++ parcial1/proyecto2/Filosofo.java
@@ -1,6 +1,4 @@
-import java.util.Random;
 import javax.swing.JLabel;
-
 
 public class Filosofo extends Thread{
 
--- parcial1/proyecto2/Main.java
+++ parcial1/proyecto2/Main.java
@@ -1,27 +1,17 @@
-
-import java.util.List;
-import javax.swing.SwingUtilities;
 import javax.swing.JFrame;
-import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 
 public class Main extends JFrame {
     public Filosofo filosofos[];
     public Tenedor tenedores[];
+    public Filosofo temporalFilosofo;
+    public Tenedor temporalTenedor;
 
-
-    public Boolean change;
     public ImageIcon filosofoImg;
     public ImageIcon tenedorImg;
 
-    public JButton crearProductorButton;
-    public JButton destroyProductorButton;
-    public JButton destroyConsumidorButton;
-    public JButton crearConsumidorButton;
     public JFrame frame;
     public JLabel label;
 
@@ -32,7 +22,6 @@ public class Main extends JFrame {
         tenedores = new Tenedor[5];
 
         frame = new JFrame();
-        change = false;
         filosofoImg = new ImageIcon("provedor.png");
         tenedorImg = new ImageIcon("tenedor.png");
 
@@ -40,9 +29,10 @@ public class Main extends JFrame {
         for(int i = 0; i < 5; i++){
             label = this.createImageLabel(i*72,tenedorImg,0);
             label.setText("IDLE");
-            Tenedor tenedor = new Tenedor(label,i);
-            tenedores[i] = tenedor;
+            temporalTenedor = new Tenedor(label,i);
+            tenedores[i] = temporalTenedor;
         }
+
 
         for(int i = 0; i < 5; i++){
             label = this.createImageLabel(i*72,filosofoImg,-36);
@@ -54,8 +44,8 @@ public class Main extends JFrame {
                 tenedor1 = 0;
             }
             label.setText("IDLE");
-            Filosofo filosofo = new Filosofo(tenedores[tenedor1],tenedores[i],"Filosofo",label,i);
-            filosofos[i]=filosofo;
+            temporalFilosofo = new Filosofo(tenedores[tenedor1],tenedores[i],"Filosofo",label,i);
+            filosofos[i]= temporalFilosofo;
         }
 
         frame.setSize(1000,1000);
@@ -63,7 +53,6 @@ public class Main extends JFrame {
         frame.setLayout(null);
         frame.setVisible(true);
     }
-
 
     public static void main(String[] args){
         Main main = new Main();
@@ -75,21 +64,8 @@ public class Main extends JFrame {
         for(int i = 0; i<5; i++){
             this.activate(i);
         }
-
-
-        while(true){
-            if(change){
-                SwingUtilities.updateComponentTreeUI(frame);
-                change = false;
-            }
-            try{
-                Thread.sleep(16);
-            }catch(InterruptedException ex){
-                ex.printStackTrace();
-            }
-        }
-
     }
+
     public void activate(int i){
         filosofos[i].start();
         try{
@@ -113,7 +89,6 @@ public class Main extends JFrame {
 
         label.setBounds(x,y,300,300);
         label.setLayout(null);
-        this.change = true;
         frame.add(label);
         return label;
     }
